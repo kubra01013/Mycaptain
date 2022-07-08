57043287b248c57698b2227e5753d9ed7718b681
kubra01013 <108573065+kubra01013@users.noreply.github.com>
2022-07-08 20:42:42 +0530

    Add files via upload

---
+++ List1 & List2.py
@@ -0,0 +1,15 @@
+#list 1
+list1 = [12,-1,5,64,-14]
+print(list1)
+for num in list1:
+    if num >= 0:
+        print(num,end =" ")
+
+
+
+#list 2
+list2 = [12,14,-95,3]
+print(list2)
+for num in list2:
+    if num >= 0:
+        print(num,end =" ")
